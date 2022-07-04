abdcede65b2ee94d9b6deebcbdb49c7c713d5f90
shamagri <108648845+shamagri@users.noreply.github.com>
2022-07-04 15:11:56 +0530

    code commit

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
+  selectTab :  any;
+  @ViewChild('tabs') tabs: IonTabs;
+
   constructor() {}
+
+  setCurrentTab(event){
+    this.selectTab = this.tabs.getSelected();
+  }
 
 }
